0902d9b2d0f232052eaed90f5bfc9f9a9bffd28b
azimsaidov <azimdsaidov@gmail.com>
2025-01-15 22:45:35 -0800

    first compiled code

--- tech.c
+++ tech.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
+void addOne(int *n) {
+    *n += 1;
+    return;
+}
+
+
 int main() {
-    printf("Trying for a fifth time!\n");
+
+    int n = 0;
+    int *pn = &n;
+    addOne(pn);
+    printf("%d\n", n);
     return 0;
 }
